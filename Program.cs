18289afc305f526c6985b8679e0543f7ce37c498
Felipe França <contatofelipefranca12@gmail.com>
2020-10-04 12:09:49 -0300

    Adiciona a conclusão do exercício HoraSistema

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HoraSistema
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("--- Data e hora do sistema atual");
+            Console.WriteLine("--- Data e hora atual do sistema");
             DateTime horaSistema = DateTime.Now;
             Console.WriteLine($"Data e hora do sistema {horaSistema}");
             Console.WriteLine("Pressione qualquer tecla para sair");
